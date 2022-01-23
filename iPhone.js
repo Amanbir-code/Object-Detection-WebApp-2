ee3aaf1d7f4ffaec89e7504bf2162c12528c7ba6
Amanbir-code <76481658+Amanbir-code@users.noreply.github.com>
2022-01-23 18:39:47 +0530

    Add files via upload

---
+++ iPhone.js
@@ -0,0 +1,31 @@
+Status = "";
+
+function preload()
+{
+    img = loadImage("Crocs.PNG")
+}
+
+function setup()
+{
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCanvas(VIDEO);
+    video.hide();
+    video.size(380, 380);
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    Status = true;
+    objectDetector.detect(video, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
